8cbde8bd0025739bba03cefcbfc39f36a531c034
OUlmachi-Yassir <oulmachi.yassir@gmail.com>
2024-01-04 11:17:26 +0100

    OLM

--- app/models/Notification.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Notification
-{
-    private $db;
-    public function __construct()
-    {
-        $this->db = new Database;
-    }
-}
--- app/models/Wallet.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Wallet
-{
-    private $db;
-    public function __construct()
-    {
-        $this->db = new Database;
-    }
-}
