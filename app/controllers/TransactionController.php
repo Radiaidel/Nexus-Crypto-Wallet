f30f18465b33e6db77ab31051e10e95fea76b69a
radiaide <idelkadiradia@gmail.com>
2024-01-05 10:27:32 +0100

    transfert avec notification et alert

--- app/controllers/TransactionController.php
+++ app/controllers/TransactionController.php
@@ -4,6 +4,9 @@ class TransactionController extends Controller
     private $Transaction;
     private $User;
     private $Wallet;
+    private $Notification;
+    private $crypto;
+
 
     public function __construct()
     {
@@ -67,30 +70,49 @@ class TransactionController extends Controller
     {
         session_start();
 
-    
+
         if (isset($_POST['transferBtn'])) {
             $userIDSender = 2; // Remplacez par le véritable ID de l'utilisateur émetteur
             $userIDRecipient = $_SESSION['UserIDRecipient'];
-            $cryptoID = $_POST['cryptoType']; // Assurez-vous que 'cryptoType' est correctement défini dans le formulaire
-            $type = 'Transfert'; // ou toute autre valeur de type nécessaire
-            $quantity = $_POST['quantity']; // Assurez-vous que 'quantity' est correctement défini dans le formulaire
-            $transactionDate = date('Y-m-d H:i:s'); // ou toute autre méthode appropriée pour obtenir la date actuelle
+            $cryptoID = $_POST['cryptoType'];
+            $type = 'Transfert';
+            $quantity = $_POST['quantity'];
+            $transactionDate = date('Y-m-d H:i:s');
 
             $this->Wallet = $this->model('Wallet');
-            $resultAdd =$this->Wallet->CryptoToRecipientWallet($userIDRecipient, $cryptoID, $quantity);
-            
+            $resultAdd = $this->Wallet->CryptoToRecipientWallet($userIDRecipient, $cryptoID, $quantity);
             $resultRemove = $this->Wallet->removeCryptoFromWallet($userIDSender, $cryptoID, $quantity);
-            if ($resultAdd && $resultRemove ) {
-                 $this->Transaction->insertTransaction($userIDSender, $userIDRecipient, $cryptoID, $type, $quantity, $transactionDate);
+            if ($resultAdd && $resultRemove) {
+                $this->Transaction->insertTransaction($userIDSender, $userIDRecipient, $cryptoID, $type, $quantity, $transactionDate);
 
-                echo 'La transaction a été enregistrée avec succès dans la base de données.';
+
+
+                $this->crypto = $this->model('Crypto');
+                $cryptoName = $this->crypto->getCryptoNameByID($cryptoID);
+
+                $this->Notification = $this->model('Notification');
+                $message = "Vous avez reçu un transfert de $quantity $cryptoName.";
+                $notificationDate = date('Y-m-d H:i:s');
+                $this->Notification->NewNotification($userIDRecipient, $message, $notificationDate);
+
+
+
+                echo '<script>
+                    alert("La transaction a été enregistrée avec succès dans la base de données.");
+                    window.location.href = "' . URLROOT . 'Pages/transfer";
+                </script>';
             } else {
-                echo 'Erreur lors de l\'enregistrement de la transaction dans la base de données.';
+                echo '<script>
+                    alert("Erreur lors de l\'enregistrement de la transaction dans la base de données.");
+                    window.location.href = "' . URLROOT . 'Pages/transfer";
+                </script>';
             }
         } else {
-            echo 'Confirmation non reçue.';
+            echo '<script>
+                alert("Confirmation non reçue.");
+                window.location.href = "' . URLROOT . 'Pages/transfer";
+            </script>';
         }
-
         exit();
     }
 }
